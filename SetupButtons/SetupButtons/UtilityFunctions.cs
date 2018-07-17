ebf9d7d5559bf5e2b5f0cd6e894b279a6e38edf6
Peter N K <pnk@avb.bg>
2018-07-17 17:12:48 +0300

    Ready for use. The test was created in version 2.0.40

---
+++ SetupButtons/SetupButtons/UtilityFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetupButtons
+{
+    class ReportAction
+    {
+        public static void PositiveReport(string Actual)
+        {
+            Ranorex.Report.Success("Sucessfully Performed", Actual);
+        }
+
+        public static void NegativeReport(string Actual, string Hardcoded)
+        {
+            string strRealText = "Actual Text is: " + Actual;
+            Ranorex.Report.Error(strRealText);
+
+            string strExpectedText = "Hardcoded Text is: " + Hardcoded;
+            Ranorex.Report.Error(strExpectedText);
+        }
+    }
+
+    class StringCopare
+    {
+        public static void Text(string Actual, string Hardcoded)
+        {
+            string RealText = Actual;
+            string HardcodedText = Hardcoded;
+
+            if (RealText == HardcodedText)
+            {
+                ReportAction.PositiveReport(RealText);
+            }
+
+            else
+                ReportAction.NegativeReport(RealText, HardcodedText);
+        }
+    }
+}
